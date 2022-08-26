78a5d3f30c6a1a2e8671e0fc6cffc505fa53a02e
dothechuyen <dtn1430a0053@tuaf.edu.vn>
2022-08-26 12:32:54 +0700

    init

--- versions/5.4.1/core/server/api/endpoints/utils/validators/input/members.js
+++
@@ -1,9 +0,0 @@
-const jsonSchema = require("../utils/json-schema");
-async function editValidate() {
-    return true;
-}
-
-module.exports = {
-    add: jsonSchema.validate,
-    edit: editValidate,
-};
